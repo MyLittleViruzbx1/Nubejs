d77b5fc3694994cfa7534090ae7d100e7fae483d
MyLittleViruzbx1 <mylittleviruz@gmail.com>
2024-05-21 16:40:05 -0600

    Respondiendo a los demas archivos en server

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,35 @@
+import fs from 'fs';
 import http from 'http';
 
 const server = http.createServer((req, res) =>{
 
-    console.log(req.url);
+    // console.log(req.url);
 
-    res.write('Hola mundo!');
-    res.end();
+    // res.writeHead(200,{'Content-Type': 'text/html'});
+    // res.write('<h1>Hola Mundo!<h1/>');
+        // res.end();
+
+    // const data = {name: 'Wilson Doe', age: 30, Departamento: 'Guatemala'};
+    // res.writeHead(200, {'Content-type': 'applicacion/json'});
+    // res.end(JSON.stringify(data) );
+
+
+    console.log(req.url);
+    if(req.url === '/') {
+        const htmlFile = fs.readFileSync('./public/index.html', 'utf-8');
+        res.writeHead(200,{'Content-Type' : 'text/html'})
+        res.end(htmlFile);
+        return;
+    }
+
+    if(req.url?.endsWith('.js') ){
+        res.writeHead(200,{'Content-Type': 'application/javascript'});
+    }else if(req.url?.endsWith('.css')){
+        res.writeHead(200,{'Content-Type': 'text/css'});
+    }
+
+    const responseContent = fs.readFileSync(`./public${ req.url}`, 'utf-8');
+    res.end( responseContent );
 
 });
 
